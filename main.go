3d6f1073a9ce3103bc085d669468dd16252fdef3
Tuan Bui <tuan.van.bui@advesa.com>
2025-08-27 19:02:47 +0700

    refactor: start read-eval-print-loop

--- main.go
+++ main.go
@@ -1,27 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex >")
-		scanner.Scan()
-		input := scanner.Text()
-		cleanInput := cleanInput(input)
-		if len(cleanInput) > 0 && len(cleanInput[0]) > 0 {
-			upperCaseFirst := strings.ToUpper(string(cleanInput[0][0]))
-			restOfWord := ""
-			if len(cleanInput[0]) > 1 {
-				restOfWord = cleanInput[0][1:]
-			}
-			textWithoutFirstWord := strings.Join(cleanInput[1:], " ")
-			fmt.Printf("You entered: %s%s %s\n", upperCaseFirst, restOfWord, textWithoutFirstWord)
-		}
-	}
+	startRepl()
 }
