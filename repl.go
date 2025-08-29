cc3e4f2e4319ff13fcc89b6562b8778195d6ea76
Tuan Bui <timeless2211>
2025-08-29 18:10:04 +0700

    refactor code structure

--- repl.go
+++ repl.go
@@ -5,91 +5,39 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/timeless2211/pokedexcli/internal/pokeapi"
 )
 
 type config struct {
-	Next     bool
-	Previous bool
+	pokeapiClient    pokeapi.Client
+	nextLocationsURL *string
+	prevLocationsURL *string
 }
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func(*config) error
-}
-
-var commands = []cliCommand{
-	{
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback:    callbackExit,
-	},
-	{
-		name:        "help",
-		description: "Show help information",
-		callback:    callbackHelp,
-	},
-	{
-		name:        "map",
-		description: "Show the map of all Pokemons",
-		callback:    callbackGetMap,
-	},
-	{
-		name:        "mapb",
-		description: "Show the previous map of all Pokemons",
-		callback:    callbackGetMap,
-	},
-}
-
-func callbackExit(*config) error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func callbackHelp(*config) error {
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:")
-	fmt.Println()
-	fmt.Println("help: Displays a help message")
-	fmt.Println("exit: Exit the Pokedex")
-	fmt.Println()
-	return nil
-}
-
-func callbackGetMap(*config) error {
-	locationAreas, error := fetchLocationAreas(&config{})
-	if error != nil {
-		return error
-	}
-	for _, area := range locationAreas.Results {
-		fmt.Println(area.(map[string]any)["name"])
-	}
-	return nil
-}
-
-func startRepl() {
-	cfg := &config{}
+func startRepl(cfg *config) {
 	reader := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
 		reader.Scan()
-		text := cleanInput(reader.Text())
-		if len(text) == 0 {
+
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
 			continue
 		}
-		textStr := strings.Join(text, " ")
 
-		for _, cmd := range commands {
-			if cmd.name == textStr {
-				if cmd.name == "map" {
-					cfg.Next = true
-				}
-				if cmd.name == "mapb" {
-					cfg.Previous = true
-				}
-				cmd.callback(cfg)
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback(cfg)
+			if err != nil {
+				fmt.Println(err)
 			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
 		}
 	}
 }
@@ -98,4 +46,35 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func(*config) error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
